d9e5291472df75b0acdb8ea5c4bdb0cb5bfd4102
Rainer Simon <aboutgeo@gmail.com>
2020-08-28 17:55:31 +0200

    Revised polyfills concerning IE11 compat

--- src/utils/index.js
+++ src/utils/index.js
@@ -51,7 +51,7 @@ const deflateNodeList = parents => {
 }
 
 /**
- * Adds MS Edge polyfills for Element.matches and .closest methods.
+ * Adds DOM polyfills that babel polyfill doesn't include
  */
 export const addPolyfills = () => {
 
@@ -70,106 +70,6 @@ export const addPolyfills = () => {
       } while (el !== null && el.nodeType === 1);
       return null;
     };
-  }
-
-  /*
-  if (!String.prototype.startsWith) {
-      Object.defineProperty(String.prototype, 'startsWith', {
-          value: function(search, rawPos) {
-              var pos = rawPos > 0 ? rawPos|0 : 0;
-              return this.substring(pos, pos + search.length) === search;
-          }
-      });
-  }
-  */
-
-  if (!String.prototype.includes) {
-    String.prototype.includes = function(search, start) {
-      'use strict';
-
-      if (search instanceof RegExp) {
-        throw TypeError('first argument must not be a RegExp');
-      }
-      if (start === undefined) { start = 0; }
-      return this.indexOf(search, start) !== -1;
-    };
-  }
-
-  if (!Array.prototype.find) {
-    Object.defineProperty(Array.prototype, 'find', {
-      value: function(predicate) {
-        // 1. Let O be ? ToObject(this value).
-        if (this == null) {
-          throw TypeError('"this" is null or not defined');
-        }
-
-        var o = Object(this);
-
-        // 2. Let len be ? ToLength(? Get(O, "length")).
-        var len = o.length >>> 0;
-
-        // 3. If IsCallable(predicate) is false, throw a TypeError exception.
-        if (typeof predicate !== 'function') {
-          throw TypeError('predicate must be a function');
-        }
-
-        // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-        var thisArg = arguments[1];
-
-        // 5. Let k be 0.
-        var k = 0;
-
-        // 6. Repeat, while k < len
-        while (k < len) {
-          // a. Let Pk be ! ToString(k).
-          // b. Let kValue be ? Get(O, Pk).
-          // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
-          // d. If testResult is true, return kValue.
-          var kValue = o[k];
-          if (predicate.call(thisArg, kValue, k, o)) {
-            return kValue;
-          }
-          // e. Increase k by 1.
-          k++;
-        }
-
-        // 7. Return undefined.
-        return undefined;
-      },
-      configurable: true,
-      writable: true
-    });
-
-  }
-
-  if (typeof Object.assign !== 'function') {
-    // Must be writable: true, enumerable: false, configurable: true
-    Object.defineProperty(Object, "assign", {
-      value: function assign(target, varArgs) { // .length of function is 2
-        'use strict';
-        if (target === null || target === undefined) {
-          throw new TypeError('Cannot convert undefined or null to object');
-        }
-
-        var to = Object(target);
-
-        for (var index = 1; index < arguments.length; index++) {
-          var nextSource = arguments[index];
-
-          if (nextSource !== null && nextSource !== undefined) {
-            for (var nextKey in nextSource) {
-              // Avoid bugs when hasOwnProperty is shadowed
-              if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-                to[nextKey] = nextSource[nextKey];
-              }
-            }
-          }
-        }
-        return to;
-      },
-      writable: true,
-      configurable: true
-    });
   }
 
 }
