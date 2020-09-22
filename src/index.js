9d9b309eb9dfc440f31c4e7898051871bf27c878
Rainer Simon <rainer.simon@ait.ac.at>
2020-09-22 08:31:50 +0200

    Minor API change

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export { default as Editor } from './editor/Editor';
-export { default as Environment } from './Environment';
+export { default as createEnvironment } from './Environment';
 export { default as I18n } from './i18n';
 export { default as TextAnnotator } from './TextAnnotator';
 export { default as WebAnnotation } from './WebAnnotation';
