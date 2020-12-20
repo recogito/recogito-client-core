f186ff8109ed9a2cbffc8439167423837f5b0025
Rainer Simon <aboutgeo@gmail.com>
2020-12-20 16:29:20 +0100

    Simplified to function component

--- src/editor/widgets/comment/PurposeDropdown.jsx
+++ src/editor/widgets/comment/PurposeDropdown.jsx
@@ -1,20 +1,37 @@
-import React, { Component } from 'preact/compat';
+import React from 'preact/compat';
 import Select from 'react-select';
 
-export const purposes = [{'value': 'assessing', 'label': 'Assessing'}, {'value': 'bookmarking', 'label': 'Bookmarking'}, {'value': 'classifying', 'label': 'Classifying'}, {'value': 'commenting', 'label': 'Commenting'}, {'value': 'describing', 'label': 'Describing'},{'value': 'editing', 'label': 'Editing'}, {'value': 'highlighting', 'label': 'Highlighting'},{'value': 'identifying', 'label': 'Identifying'},{'value': 'linking', 'label': 'Linking'},{'value': 'moderating', 'label': 'Moderating'},{'value': 'questioning', 'label': 'Questioning'},{'value': 'replying', 'label': 'Replying'}]
+export const purposes = [
+  {'value': 'assessing', 'label': 'Assessing'}, 
+  {'value': 'bookmarking', 'label': 'Bookmarking'}, 
+  {'value': 'classifying', 'label': 'Classifying'}, 
+  {'value': 'commenting', 'label': 'Commenting'}, 
+  {'value': 'describing', 'label': 'Describing'},
+  {'value': 'editing', 'label': 'Editing'}, 
+  {'value': 'highlighting', 'label': 'Highlighting'},
+  {'value': 'identifying', 'label': 'Identifying'},
+  {'value': 'linking', 'label': 'Linking'},
+  {'value': 'moderating', 'label': 'Moderating'},
+  {'value': 'questioning', 'label': 'Questioning'},
+  {'value': 'replying', 'label': 'Replying'}
+]
 
-export default class PurposeDropdown extends Component {
-  render() {
-    const selectedOption = this.props.content ? {'value': this.props.content, 'label': this.props.content.charAt(0).toUpperCase() + this.props.content.slice(1) } : undefined;
-    return (
-      <div class="r6o-purposedropdown">
-        <Select
-          value={selectedOption}
-          onChange={this.props.onChange}
-          options={purposes}
-          isDisabled={!this.props.editable}
-        />
-      </div>
-    );
-  }
-} 
+const PurposeDropdown = props => {
+
+  const selectedOption = this.props.content ?
+    { 'value': this.props.content, 'label': this.props.content.charAt(0).toUpperCase() + this.props.content.slice(1) } : undefined;
+
+  return (
+    <div class="r6o-purposedropdown">
+      <Select
+        value={selectedOption}
+        onChange={this.props.onChange}
+        options={purposes}
+        isDisabled={!this.props.editable}
+      />
+    </div>
+  );
+
+}
+
+export default PurposeDropdown;
