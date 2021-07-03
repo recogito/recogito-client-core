ed436ec8ec42c3edc30b61ba825e872b8b09127e
Rainer Simon <aboutgeo@gmail.com>
2021-07-03 17:55:31 +0200

    Removed polyfills

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,30 +1,6 @@
 import I18n from '../i18n';
 
 /**
- * Adds DOM polyfills that babel polyfill doesn't include
- */
-export const addPolyfills = () => {
-
-  if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype.msMatchesSelector ||
-      Element.prototype.webkitMatchesSelector;
-  }
-
-  if (!Element.prototype.closest) {
-    Element.prototype.closest = function(s) {
-      let el = this;
-
-      do {
-        if (Element.prototype.matches.call(el, s)) return el;
-        el = el.parentElement || el.parentNode;
-      } while (el !== null && el.nodeType === 1);
-      return null;
-    };
-  }
-
-}
-
-/**
  * Helper to init the i18n class with a pre-defined or auto-detected locale.
  */
 export const setLocale = (locale, opt_messages) => {
